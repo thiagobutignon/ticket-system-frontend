e68d1c6d3085b83667e82412a1926e533143f8c7
thiagobutignon <thiago.butignon@townsq.com.br>
2025-02-27 18:33:59 -0300

    initial commit

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: 'https://ticket-assignment-system-backend-thiago-butignon-jatot0d87.vercel.app',
+  },
+};
+
+export default nextConfig;
